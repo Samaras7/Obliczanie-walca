6c5872e885e52bb0792d1e295335ec66fd4f731c
Samaras7 <patryk.piworowicz@gmail.com>
2021-12-06 16:04:02 +0100

    Dodaj pliki projektów.

---
+++ Obliczanie walca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1._1
+{
+    class Program
+    {
+        static double Walec(double r, double h)
+        {
+            double V = Math.PI * r * r * h;
+            return V;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj promień: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("podaj wysokość: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Walec(r, h));
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
